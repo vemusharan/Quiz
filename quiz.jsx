d906d03b01ef7830e0fb2f7331f1ac2e740d74bd
sharan <sai.vemu@st.ovgu.de>
2020-12-22 01:55:43 +0100

    Question 1 Navigation Working

--- quiz.jsx
+++ quiz.jsx
@@ -3,22 +3,38 @@ class Quiz extends React.Component {
         super(props);
         this.state ={list:props.list}
         window.$inst = this.state.list.instruction;
-        window.$title = "Instruction";
+        window.$title = "Instructions";
+        window.$check = true;
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleNext = this.handleNext.bind(this);
+        window.$listitems = this.state.list.instruction.map((t) => <li>   {t}  </li> );
+
+        window.$button = <input type='submit' value='Start Quiz' onSubmit= {this.handleSubmit} />;
+        window.$onsubmit = this.handleSubmit ;
         
-   
+
+        console.log(this.state.list.question.question1)
+        console.log(this.state.list.question.Options)
+        
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        window.$title = this.state.list.question.question1;
+        window.$listitems = this.state.list.question.Options.map((t) => <li> <input name="isGoing" type="radio"/>   {t}  </li> );
+        window.$button = <input type='submit' value='Next Question' onSubmit= {this.handleNext} />;
+        window.$onsubmit = this.handleNext;
+        this.setState({list:this.state.list});
+
+        //const listitems = this.state.list.instruction.map((t) => <li> <input name="isGoing" type="checkbox"/>   {t}  </li> );
     }
 
 
-    handleSubmit(event) {
-        event.preventDefault();
-        console.log("hello");
-        this
-        this.state.list.instruction= ["Questionn1"];
-        window.$title = "Questions Number 1"
-        this.setState({list:this.state.list});
-        <div> working </div> ;
 
+    handleNext(event) {
+        event.preventDefault();
+        window.$title = "Question 2";
+        window.$listitems = " ";
+        this.setState({list:this.state.list});
 
     }
 
@@ -26,15 +42,28 @@ class Quiz extends React.Component {
 
 
     render() {
+        
     
-        const listitems = this.state.list.instruction.map((t) => <li> {t}  </li> );
         return (
             <div>
             <h1>  {this.props.list.title} </h1>
-            <div id="imp">  <h2>  {window.$title}   </h2> <ol>  {listitems} </ol> <h4> &nbsp;  <i> Goodluck !!! Bigboss :) </i> </h4> </div>
-           <form onSubmit={this.handleSubmit}>
-           <input type='submit' value='Start Quiz' onSubmit= {this.handleSubmit} />
+
+            <h3> {window.$title}  </h3>
+ 
+
+           <ol>  {window.$listitems}  </ol>
+
+
+           <form onSubmit={window.$onsubmit}>
+           {window.$button}
            </form>
+
+
+
+
+            &nbsp; &nbsp;
+          <h4> <b>  <i> Good luck ! Bigbosss :) </i> </b> </h4> 
+
             </div>
         );
     }
@@ -44,13 +73,20 @@ class Quiz extends React.Component {
 
 
 
+  
 
 
+
+const question1 = {question1:"Question 1 :  How many times did Abhijit was Nominated?",Options:["11","12","10"]}
+const question2 = {}
 
 const ins = ["The Quiz consists of 5 Question each Question carry 4 marks", " Each Question has Equal points ", "There is no time limit but dont waste your time","U already wasted so much time watching bigboss if u cannot even remember thes then its an insult to bigboss and u"," Jai Sohel! Katha vere undali mari :) "]
-const quiz = {title:"Bigboss Season 4 Quiz",instruction:ins}
+const quiz = {title:"Bigboss Season 4 Quiz",instruction:ins,question:question1}
 
 ReactDOM.render(
     <Quiz list={quiz} /> ,
     document.getElementById('root')
 );
+
+
+
